c2e4db2f729d6931c705ff8f687f288abfa8a6d3
Robotz213 <nicholas@robotz.dev>
2025-06-18 18:19:09 -0300

    ✨ feat: Enhance CardsView component with new features
    src/views/funcionarios/components/CardsView.vue: Refactor data handling and improve item selection logic. Add FontAwesome icons for better UI representation. Update DataTable structure to include additional columns and options.

--- src/views/funcionarios/components/CardsView.vue
+++ src/views/funcionarios/components/CardsView.vue
@@ -1,26 +1,31 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
 
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import DataTablesCore from "datatables.net-bs5";
 import DataTable from "datatables.net-vue3";
+import { watch } from "vue";
 DataTable.use(DataTablesCore);
 
-const data = [
-  [1, 5]
-]
-
-const list = [
-  { msg: "Bruce Lee" },
-  { msg: "Jackie Chan" },
-  { msg: "Chuck Norris" },
-  { msg: "Jet Li" },
-  { msg: "Kung Fury" },
-];
+const data = ref<Array<string[]>>([]);
+const selectedItem = ref("");
+const list = [{ msg: "Departamento" }, { msg: "Cargo" }, { msg: "Setor" }, { msg: "Empresa" }];
 
 const query = ref("");
 
 const computedList = computed(() => {
-  return list.filter((item) => item.msg.toLowerCase().includes(query.value));
+  return list.filter((item) => item.msg.toLowerCase().includes(query.value.toLowerCase()));
+});
+
+function classListItem(item: string) {
+  console.log(selectedItem.value);
+
+  return selectedItem.value === item ? "list-group-item active" : "list-group-item";
+}
+
+watch(selectedItem, () => {
+  data.value = [["nicholas azevedo", "mail.google.com", "009"]];
 });
 </script>
 
@@ -40,9 +45,10 @@ const computedList = computed(() => {
         <div class="card-body">
           <TransitionGroup name="slide-fade" class="list-group" tag="ul">
             <li
-              class="list-group-item list-group-item-action"
+              :class="classListItem(item.msg)"
               v-for="(item, index) in computedList"
               :key="index"
+              @click="selectedItem = item.msg"
             >
               {{ item.msg }}
             </li>
@@ -50,16 +56,38 @@ const computedList = computed(() => {
         </div>
       </div>
     </div>
-    <div class="col-md-9 row card p-3">
-      <div class="card-body rounded border border-circle">
-        <DataTable :data="data" class="display table table-striped">
-          <thead>
-            <tr>
-              <th>A</th>
-              <th>B</th>
-            </tr>
-          </thead>
-        </DataTable>
+    <div class="col-md-9">
+      <div class="card" style="height: 75dvh">
+        <div class="card-header d-flex justify-content-between">
+          <span class="fw-semibold"> Lista De Funcionários </span>
+          <button class="btn btn-sm btn-outline-primary ms-auto">
+            <span class="d-flex allign-items-center">
+              <FontAwesomeIcon
+                :icon="faPlus"
+                class="me-1 rounded border border-1 p-1 border-primary"
+              />
+              <span class="align-self-center">Cadastrar Funcionário</span>
+            </span>
+          </button>
+        </div>
+        <div class="card-body">
+          <DataTable
+            :data="data"
+            class="display table table-striped table-hover"
+            :options="{
+              pageLength: 10,
+              lengthChange: false, // Remove a opção de alterar quantidade por página
+            }"
+          >
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Código Identificação</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+          </DataTable>
+        </div>
       </div>
     </div>
   </div>
