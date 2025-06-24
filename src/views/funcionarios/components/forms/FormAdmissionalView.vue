3c51d5ffd5209434f00207009b48a0056d52a4d2
Robotz213 <nicholas@robotz.dev>
2025-06-24 17:17:12 -0400

    ✨ feat: Enhance admission form functionality
    src/views/funcionarios/components/forms/FormAdmissionalView.vue: Refactor form handling, add reactive form state, and improve error handling for API submissions. Update file input to accept PDF files and ensure date input has a minimum date restriction.

--- src/views/funcionarios/components/forms/FormAdmissionalView.vue
+++ src/views/funcionarios/components/forms/FormAdmissionalView.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
-import { pinia } from "@/main";
+import { api, pinia } from "@/main";
 import admissionalStore from "@/stores/admissional";
-import { BButton, BFormFile, BFormGroup, BFormInput, BModal } from "bootstrap-vue-next";
+import { isAxiosError } from "axios";
+import { BButton, BFormFile, BFormGroup, BFormInput, BModal, useModal } from "bootstrap-vue-next";
 import { storeToRefs } from "pinia";
-import { ref } from "vue";
+import { reactive, ref } from "vue";
+
+const { hide: HideModal } = useModal("FormAdmissional");
+const Form = reactive({
+  contrato: null,
+  prazo: "",
+});
 
 const file = ref(null);
-
+const overlayAdmissional = ref(false);
 const { cellFuncionario } = storeToRefs(admissionalStore(pinia));
+const opacity = ref(0.18);
 
-async function handleSubmit(e: Event) {
-  e.preventDefault();
-}
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const selected = ref(null);
 
@@ -20,6 +25,30 @@ const exFieldNamesOptions = [
   { item: "A", name: "Option A" },
   { item: "B", name: "Option B" },
 ];
+
+async function handleSubmit(e: Event) {
+  e.preventDefault();
+  overlayAdmissional.value = true;
+  let message = "Erro ao cadastrar funcionário!";
+  try {
+    const response = await api.post("/forms/funcionario/admissional", Form);
+
+    message = response.data.message || "Erro ao cadastrar funcionário";
+  } catch (err) {
+    if (isAxiosError(err)) {
+      if (err.response?.data && err.response.data.message) {
+        message = err.response.data.message;
+      }
+    }
+  }
+  alert(message);
+  HideModal();
+  overlayAdmissional.value = false;
+}
+
+const minDate = () => {
+  return new Date(Date.now() + 3 * 24 * 60 * 60 * 1000).toISOString().split("T")[0];
+};
 </script>
 
 <template>
@@ -30,11 +59,13 @@ const exFieldNamesOptions = [
     @hide="cellFuncionario = ['']"
     :title="`Admissão Funcionário -- ${cellFuncionario[0]} --`"
     footer-class="d-flex"
+    no-footer
   >
-    <form @submit="handleSubmit">
-      <div class="row g-1">
-        <div class="col-12 d-flex flex-column gap-4">
-          <!-- <BFormSelect
+    <BOverlay class="p-3" :show="overlayAdmissional" :opacity="opacity" rounded="md">
+      <form @submit="handleSubmit">
+        <div class="row g-1">
+          <div class="col-12 d-flex flex-column gap-4">
+            <!-- <BFormSelect
             size="md"
             v-model="selected"
             :options="exFieldNamesOptions"
@@ -47,31 +78,42 @@ const exFieldNamesOptions = [
               </BFormSelectOption>
             </template>
           </BFormSelect> -->
-          <div>
-            <BFormFile size="md" v-model="file" label="Contrato de Trabalho" label-class="fw-bold">
-            </BFormFile>
+            <div>
+              <BFormFile
+                size="md"
+                v-model="file"
+                label="Contrato de Trabalho"
+                label-class="fw-bold"
+                accept=".pdf"
+              >
+              </BFormFile>
+            </div>
+            <BFormGroup
+              id="fieldset-prazo"
+              label="Data Prazo"
+              label-for="input-floating-prazo"
+              label-class="mb-5"
+              floating
+            >
+              <BFormInput
+                type="date"
+                id="input-floating-prazo"
+                :state="null"
+                trim
+                placeholder="..."
+                :min="minDate()"
+                v-model="Form.prazo"
+              />
+            </BFormGroup>
           </div>
-          <BFormGroup
-            id="fieldset-prazo"
-            label="Data Prazo"
-            label-for="input-floating-prazo"
-            label-class="mb-5"
-            floating
-          >
-            <BFormInput
-              type="date"
-              id="input-floating-prazo"
-              :state="null"
-              trim
-              placeholder="..."
-            />
-          </BFormGroup>
         </div>
-      </div>
-    </form>
-
-    <template #footer>
-      <BButton type="submit" variant="success" size="lg"> Salvar </BButton>
-    </template>
+        <hr class="mt-5" />
+        <div class="d-flex flex-column">
+          <BButton type="submit" variant="success" size="md">
+            <strong> Salvar </strong>
+          </BButton>
+        </div>
+      </form>
+    </BOverlay>
   </BModal>
 </template>
